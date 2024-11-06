56dd8f1dad2a8cc5261ae5502aba2f92d51e7a68
shreyaspatil <patil.shreyas@northeastern.edu>
2024-11-06 10:13:51 -0500

    Initial Commit

---
+++ Vision3D/Vision3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision3DApp.swift
+//  Vision3D
+//
+//  Created by Shreyas Patil on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Vision3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XCVision/XCVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCVisionApp.swift
+//  XCVision
+//
+//  Created by Shreyas Patil on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct XCVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
