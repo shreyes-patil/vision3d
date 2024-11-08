fc81a7b54ba25d01824e12d8048bded13110a075
shreyaspatil <patil.shreyas@northeastern.edu>
2024-11-08 10:39:41 -0500

    gi

---
+++ Shared/XCVision/XCVisionApp.swift
@@ -0,0 +1,20 @@
+//
+//  XCVisionApp.swift
+//  XCVision
+//
+//  Created by Shreyas Patil on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct XCVisionApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
