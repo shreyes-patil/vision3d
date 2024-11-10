911e94408c94c0294148f72ec914daa99caa50a2
shreyaspatil <patil.shreyas@northeastern.edu>
2024-11-10 18:56:45 -0500

    AR Model upload, save and view from files

--- Vision3D/Observables/InventoryFormVM.swift
+++ Vision3D/Observables/InventoryFormVM.swift
@@ -17,6 +17,7 @@ class InventoryFormViewModel: ObservableObject {
     let db = Firestore.firestore()
     let formType: FormType
     
+    
     let id : String
     @Published var name = ""
     @Published var quantity = 0
@@ -25,6 +26,15 @@ class InventoryFormViewModel: ObservableObject {
     
     @Published var loadingState: LoadingType = .none
     @Published var error: String?
+    @Published var uploadProgress: UploadProgress?
+    @Published var showUSDZSource: Bool = false
+    @Published var selectedUSDZSource: USDZSourceType?
+    
+    let byteCountFormatter: ByteCountFormatter = {
+        let f = ByteCountFormatter()
+        f.countStyle = .file
+        return f
+    }()
     
     var navigationTitle: String {
         switch formType {
@@ -36,9 +46,9 @@ class InventoryFormViewModel: ObservableObject {
     init(formType: FormType = .add) {
         self.formType = formType
         switch formType {
-            case .add:
+        case .add:
             id = UUID().uuidString
-            case .edit(let item):
+        case .edit(let item):
             id = item.id
             name = item.name
             quantity = item.quantity
@@ -76,11 +86,59 @@ class InventoryFormViewModel: ObservableObject {
             self.error = error.localizedDescription
             throw error
         }
-    
-        
     }
-
-}
+        
+        @MainActor
+        func uploadUSDZ(fileURL: URL, isSecurityScopedResource: Bool = false) async {
+            if isSecurityScopedResource, !fileURL.startAccessingSecurityScopedResource() {
+                return
+            }
+            guard let data = try? Data(contentsOf: fileURL) else { return }
+            if isSecurityScopedResource {
+                fileURL.stopAccessingSecurityScopedResource()
+            }
+            uploadProgress = .init(fractionCompleted: 0, totalUnitCount: 0, completedUnitCount: 0)
+            loadingState = .uploading(.usdz)
+            
+            defer { loadingState = .none }
+            do {
+                /// Upload USDZ to Firebase Storage
+                let storageRef = Storage.storage().reference()
+                let usdzRef = storageRef.child("\(id).usdz")
+                
+                _ = try await usdzRef.putDataAsync(data, metadata: .init(dictionary: ["contentType": "model/vnd.usd+zip"])) { [weak self] progress in
+                    guard let self, let progress else { return }
+                    self.uploadProgress = .init(fractionCompleted: progress.fractionCompleted, totalUnitCount: progress.totalUnitCount, completedUnitCount: progress.completedUnitCount)
+                }
+                let downloadURL = try await usdzRef.downloadURL()
+                
+                /// Generate Thumbnail
+                let cacheDirURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+                let fileCacheURL = cacheDirURL.appending(path: "temp_\(id).usdz")
+                try? data.write(to: fileCacheURL)
+                
+                let thumbnailRequest = QLThumbnailGenerator.Request(fileAt: fileCacheURL, size: .init(width: 300, height: 300), scale: UIScreen.main.scale, representationTypes: .all)
+                
+                if let thumbnail = try? await QLThumbnailGenerator.shared.generateBestRepresentation(for: thumbnailRequest),
+                   let jpgData = thumbnail.uiImage.jpegData(compressionQuality: 0.5) {
+                    loadingState = .uploading(.thumbnail)
+                    let thumbnailRef = storageRef.child("\(id).jpg")
+                    _ = try? await thumbnailRef.putDataAsync(jpgData, metadata: .init(dictionary: ["contentType": "image/jpeg"]), onProgress: { [weak self] progress in
+                        guard let self, let progress else { return }
+                        self.uploadProgress = .init(fractionCompleted: progress.fractionCompleted, totalUnitCount: progress.totalUnitCount, completedUnitCount: progress.completedUnitCount)
+                    })
+                    
+                    if let thumbnailURL = try? await thumbnailRef.downloadURL() {
+                        self.thumbnailURL = thumbnailURL
+                    }
+                }
+                
+                self.usdzURL = downloadURL
+            } catch {
+                self.error = error.localizedDescription
+            }
+        }
+    }
 
 enum FormType: Identifiable {
     
@@ -100,4 +158,20 @@ enum FormType: Identifiable {
 enum LoadingType: Equatable{
     case none
     case savingItem
+    case uploading(UploadType)
+}
+
+
+enum USDZSourceType{
+    case fileImporter, objectCapture
+}
+
+enum UploadType: Equatable{
+    case usdz, thumbnail
+}
+
+struct UploadProgress{
+    var fractionCompleted: Double
+    var totalUnitCount: Int64
+    var completedUnitCount: Int64
 }
--- Vision3D/Views/InventoryFormView.swift
+++ Vision3D/Views/InventoryFormView.swift
@@ -5,7 +5,9 @@
 //  Created by Shreyas Patil on 11/9/24.
 //
 
+import SafariServices
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct InventoryFormView: View {
     
@@ -16,6 +18,7 @@ struct InventoryFormView: View {
         Form{
             List{
                 inputSection
+                arSection
             }
         }
         .toolbar{
@@ -39,10 +42,31 @@ struct InventoryFormView: View {
                 .disabled(vm.loadingState != .none || vm.name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
             }
         }
+        
+        .confirmationDialog("Add USDZ", isPresented: $vm.showUSDZSource, titleVisibility: .visible,
+                        actions: {
+            Button("Select File"){
+                vm.selectedUSDZSource = .fileImporter
+            }
+            Button("Object Capture"){
+                vm.selectedUSDZSource = .objectCapture
+            }
+            
+        })
+        .fileImporter(isPresented: .init(get: { vm.selectedUSDZSource == .fileImporter }, set: { _ in
+                    vm.selectedUSDZSource = nil
+                }), allowedContentTypes: [UTType.usdz], onCompletion: { result in
+                    switch result {
+                    case .success(let url):
+                        Task { await vm.uploadUSDZ(fileURL: url, isSecurityScopedResource: true) }
+                    case .failure(let failure):
+                        vm.error = failure.localizedDescription
+                    }
+                })
         .alert(isPresented: .init(get: {vm.error != nil}, set: { _ in
             vm.error = nil
         }), error: "An error has ocured", actions: {
-            _ in 
+            _ in
             
         }, message: { _ in
             Text(vm.error ?? "")
@@ -56,6 +80,80 @@ struct InventoryFormView: View {
             Stepper("Quantity \(vm.quantity)", value : $vm.quantity)
         }
         .disabled(vm.loadingState != .none)
+    }
+    
+    var arSection : some View{
+        Section("AR Model"){
+            if let thumbnailURL = vm.thumbnailURL{
+                AsyncImage(url: thumbnailURL){
+                    phase in
+                    switch phase {
+                    case .success(let image):
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: .infinity, maxHeight: 300)
+                    
+                    case .failure:
+                        Text("failed to fetch thumbnail")
+                            .foregroundColor(.red)
+                    default: ProgressView()
+                            
+                    }
+                }
+                .onTapGesture {
+                    guard let usdzURL = vm.usdzURL else { return }
+                    viewAR(url: usdzURL)
+                }
+                
+            }
+            if let usdzURL = vm.usdzURL{
+                Button {
+                    viewAR(url: usdzURL)
+                } label: {
+                    HStack{
+                        Image(systemName: "arkit").imageScale(.large)
+                        Text("View in AR")
+                    }
+                }
+
+            } else {
+                Button{
+                    vm.showUSDZSource = true
+                } label: {
+                    HStack{
+                        Image(systemName: "arkit").imageScale(.large)
+                        Text("Add USDZ")
+                    }
+                }
+            }
+            if let progress = vm.uploadProgress,
+               case let .uploading(type) = vm.loadingState,
+               progress.totalUnitCount > 0{
+                VStack{
+                    Text("Uploading \(type == .usdz ? "USDZ" : "Thumbnail") file \(Int(progress.fractionCompleted * 100))%...")
+                    ProgressView(value: progress.fractionCompleted)
+                }
+                
+                Text("\(vm.byteCountFormatter.string(fromByteCount: progress.completedUnitCount))/ \(vm.byteCountFormatter.string(fromByteCount: progress.totalUnitCount))")
+            }
+        }
+        .disabled(vm.loadingState != .none)
+    }
+    
+    func viewAR(url: URL){
+        let SafariVC = SFSafariViewController(url: url)
+        let vc = UIApplication.shared.firstKeyWindow?.rootViewController?.presentedViewController ?? UIApplication.shared.firstKeyWindow?.rootViewController
+        
+        vc?.present(SafariVC, animated: true)
+    }
+}
+
+extension UIApplication{
+    var firstKeyWindow: UIWindow? {
+        UIApplication.shared.connectedScenes
+            .compactMap{ $0 as? UIWindowScene}
+            .filter {$0.activationState == .foregroundActive}
+            .first?.keyWindow
     }
 }
 
