489864029c959cb289bbf7214d0e8035c9d54f7f
shreyaspatil <patil.shreyas@northeastern.edu>
2024-11-11 06:43:22 -0500

    list view for visionOS

---
+++ XCVision/Views/InventoryListView.swift
@@ -0,0 +1,18 @@
+//
+//  InventoryListView.swift
+//  XCVision
+//
+//  Created by Shreyas Patil on 11/10/24.
+//
+
+import SwiftUI
+
+struct InventoryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InventoryListView()
+}
